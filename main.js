9ea00e819a6bf7ce74aea4638dc02977f3300da1
freethewalruses <148225565+freethewalruses@users.noreply.github.com>
2026-04-16 03:41:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 350,
+    height: 350,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: false,
+    skipTaskbar: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+// Allow the renderer to close or minimise the window
+ipcMain.on('close-window', () => win.close());
+ipcMain.on('minimise-window', () => win.minimize());
